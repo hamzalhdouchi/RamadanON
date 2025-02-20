612fc9ab5ee7989da0589082ea2183688aeca6f2
hamzalhadouchi <hamzabniss16@gmail.com>
2025-02-20 14:11:50 +0000

    Ajout de la méthode creatComment() pour gérer l'ajout d'un commentaire

--- RamadanON/app/Http/Controllers/commentController.php
+++ RamadanON/app/Http/Controllers/commentController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class commentController extends Controller
 {
-    //
+    public function creatComment(Request $request)
+    {
+        $validated = $request->validate([
+            'name_user' => 'required|string|max:255',
+            'content' => 'required|string|min:3',
+            'recipe_id' => 'required'
+        ]);
+
+        $comment = new Comments();
+        $comment->name_user = $validated['name_user'];
+        $comment->recipe_id = $validated['recipe_id'];
+        $comment->content = $validated['content'];
+        $comment->save();
+
+        session()->flash('success', 'Votre commentaire a été ajouté avec succès.');
+        return redirect()->route('index');
+    }
+
 }
