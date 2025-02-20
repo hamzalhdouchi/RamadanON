7085fdaf26eef24bd78df738b4352b541b559f05
hamzalhadouchi <hamzabniss16@gmail.com>
2025-02-20 14:08:36 +0000

    Ajout du modèle Recipe sans relations

--- RamadanON/app/Models/recipes.php
+++ RamadanON/app/Models/recipes.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class recipes extends Model
 {
     use HasFactory;
+    protected $fillable = ['title', 'description', 'prep_time', 'servings', 'difficulty', 'category_id', 'image'];
 }
