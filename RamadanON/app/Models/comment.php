7a079b9e2789fbfb5a73607814e9180b9cd1913a
hamzalhadouchi <hamzabniss16@gmail.com>
2025-02-20 14:03:28 +0000

    Ajout du modèle Comment sans relation

--- RamadanON/app/Models/comment.php
+++ RamadanON/app/Models/comment.php
@@ -5,7 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class comment extends Model
+class Comment extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name_user', 'recipe_id', 'content'];
+
 }
