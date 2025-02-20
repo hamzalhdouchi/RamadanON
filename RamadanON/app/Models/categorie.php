af48aaf7fad89ebac32b14a50790e93c3dc3d7fa
hamzalhadouchi <hamzabniss16@gmail.com>
2025-02-20 14:05:32 +0000

    Ajout de la relation belongsTo entre Catrgorie et Recipe

--- RamadanON/app/Models/categorie.php
+++ RamadanON/app/Models/categorie.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class categorie extends Model
 {
     use HasFactory;
+    public function recipes()
+    {
+        return $this->hasMany(recipes::class, 'category_id');
+    }
 }
